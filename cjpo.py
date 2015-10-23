159ecbf27a0fd4814e346149147b34eba2685c69
Michaël Roy <microygh@gmail.com>
2015-10-23 17:55:29 +0200

    Cleanup.

--- cjpo.py
+++ cjpo.py
@@ -1,60 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Application cliente JPO IUT Auxerre
 #
-# 
+# Client side of the NetworkDemo application
 #
-# (c)2011eB@uB
 
-# python JPOclnt.py server_address port_number
-
-import socket  # networking module
+#
+# External dependencies
+#
+import socket
 import sys
 from Tkinter import *
 
-# Création de la fenêtre graphique
+# Graphical user interface
 root = Tk()
 sw = root.winfo_screenwidth()
 sh = root.winfo_screenheight()
-print "screen width  =", sw
-print "screen height =", sh 
-# set to full screen use this:
-root.geometry("%dx%d+0+0" % (sw, sh))
+#print( 'Screen width  = {}'.format( sw ) )
+#print( 'Screen height = {}'.format( sh ) )
 
-can = Canvas(root, width =sw, height=sh-100, bg="white")
+# Set to full screen
+root.geometry( '{}x{}+0+0'.format( sw, sh ) )
+
+can = Canvas( root, width =sw, height=sh-100, bg='white' )
 can.pack()
-balle = can.create_oval(0,0,0,0, fill='red')
-Button(root, text='Quitter', command =root.quit).pack(side =BOTTOM, pady =30)
+balle = can.create_oval( 0, 0, 0, 0, fill = 'red' )
+Button( root, text = 'Quit', command = root.quit ).pack( side = BOTTOM, pady = 30 )
 
-# create Internet TCP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create Internet TCP socket
+s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
-host = sys.argv[1]  # server address
-port = int(sys.argv[2])  # server port
+# Server address
+host = sys.argv[ 1 ]
 
-try:
-    s.connect((host, port))
-except socket.error:
-    print "La connexion a échoué."
-    sys.exit()    
-print "Connexion établie avec le serveur."    
+# Server port
+port = int( sys.argv[ 2 ] )
 
-while 1:
-    msg = s.recv(256)
-    if not msg: break 
-    print "trame %s" % msg
-    (car,sep,cdr) = msg.partition(".")
-    (sx,sep,sy) = car.partition(",")
-    x = int(sx)
-    y = int(sy)
-    print "x=%d et y=%d" % (x,y)
-    can.coords(balle, x-30, y-30, x+30, y+30)
-    root.update()
+# Network connection
+try :
 
-#root.mainloop()
+	s.connect( ( host, port ) )
 
-print "Connexion interrompue."
+except socket.error :
+
+	print( 'Connection failed...' )
+	sys.exit()    
+
+print( 'Connected to the server...' )
+
+# Main loop
+while 1 :
+
+	msg = s.recv( 256 )
+	if not msg : break
+#	print( 'Frame {}'.format( msg ) )
+	car, sep, cdr = msg.partition( '.' )
+	sx, sep, sy = car.partition( ',' )
+	x = int( sx )
+	y = int( sy )
+#	print( 'x = {}  ~  y = {}'.format( x, y ) )
+	can.coords( balle, x-30, y-30, x+30, y+30 )
+	root.update()
+
+print( 'Connection lost...' )
 s.close()
 
         
--- sjpo.py
+++ sjpo.py
@@ -1,104 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Application serveur JPO IUT Auxerre
 #
-# Le programme "serveur" accepte les connexions d'une liste prédéfinie de clients possibles
-# et leur donne la main à tour de rôle, un peu comme un jeton dans un "token ring".
+# Server side of the NetworkDemo application
 #
-# (c)2010eB@uB
 
-# python JPOsrvr.py port_number
-
-import socket # Module réseau local TCP/IP
-import thread # Module de gestion des sous-processus
+#
+# External dependencies
+#
+import socket
 import sys
-from time import sleep
+import time
 
-# Message de bienvenue...
-print "Bienvenue dans l'application JPO Auxerre: coté serveur"
+# Set up Internet TCP socket
+lstn = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
-# set up Internet TCP socket
-lstn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Server port number
+port = int( sys.argv[ 1 ] )
 
-port = int(sys.argv[1])  # server port number
-# bind lstn socket to this port 
-lstn.bind(('', port))
-# start listening for contacts from clients
-lstn.listen(5)
+# Bind lstn socket to this port 
+lstn.bind( ( '', port ) )
 
-# Liste des IP des clients
+# Start listening for contacts from clients
+lstn.listen( 5 )
+
+# Client list
 hosts = []
 
-# Nombre de clients requis
-N=2
+# Required client number
+N = 2
 
-# Clients connectés
-chosts = []
-clock = thread.allocate_lock()  
-
-# Ecoute des demandes de connexion des clients tant que tous ne sont
-# pas connectés
-
+# Client connection list
 liste_client = {}
 
-while len(hosts)<N:
-	(clnt,ap) = lstn.accept()
+# Client connection
+while len( hosts ) < N :
+	
+	clnt, ap = lstn.accept()
 	client = clnt.getpeername()
-	print "Demande de connexion ",client
+	print( 'Client {}:{} connected...'.format( *client ) )
 	hosts.append( client )
-	# Teste si le client est un client autorisé
-	if client in hosts:
-		print( 'Demande de connexion de {}:{}'.format( *client ) )
-		# Teste si le client est déjà connecté
-		if client in chosts: 
-			print "Client déjà connecté"
-			print "Demande de connexion rejetée"
-		else:
-			print "Demande acceptée sur Port %s" % client[1]
-			liste_client[client]=clnt
-	else:
-		print "Tentative de connexion d'un poste inconnu "+client[0]
-		print "Demande de connexion rejetée"
+	liste_client[ client ] = clnt
 
-print "Tous les clients sont connectés, l'application est opérationelle"
-
-# Fermeture du port d'écoute principal
+# Close the server
 lstn.close()
 
-
+# Screen size
 sw = 1920
 sh = 1200
-# test
-print "screen width  =", sw
-print "screen height =", sh 
 
-# initialisation des coordonnées, des vitesses et du témoin d'animation :    
+# Ball coordinates and speed
 x, y, v, dx, dv = 50, 50, 0, 12, 5
+
+# Border
 sh = sh - 100 - 30
 
-while 1:
-#   print "Calcul de l'itération suivante"
-   xp, yp = x, y            # mémorisation des coord. précédentes
-   # déplacement horizontal :
-   if x > N*sw or x < 0 :     # rebond sur les parois latérales :
-      dx = -dx             # on inverse le déplacement
-   x = x + dx
-   # variation de la vitesse verticale (toujours vers le bas):
-   v = v + dv
-   # déplacement vertical (proportionnel à la vitesse)
-   y = y + v       
-   if y > sh:              # niveau du sol à 240 pixels : 
-      y = sh             #  défense d'aller + loin !
-      v = -v               # rebond : la vitesse s'inverse
-   n=0
-   for chost in hosts:
-      print( 'Coordonnées ({},{}) envoyées à {}:{}'.format( x, y, *chost ) )
-      clnt=liste_client[chost]
-      clnt.send("%d,%d.\n" % (x-n*sw,y))
-      n=n+1
-   sleep(0.03)
+# Main loop
+while 1 :
 
-# Attente de la déconnexion de tous les clients 
-while len(chosts) > 0: pass
-print "Done"
+	# Horizontal move
+	if x > N*sw or x < 0 :     # rebond sur les parois latérales :
+		dx = -dx             # on inverse le déplacement
+	x = x + dx
+
+	# Vertical speed variation
+	v = v + dv
+
+	# Vertical move
+	y = y + v 
+	if y > sh :              # niveau du sol à 240 pixels : 
+		y = sh             #  défense d'aller + loin !
+		v = -v               # rebond : la vitesse s'inverse
+
+	# Send ball coordinates to the clients
+	for n, chost in enumerate( hosts ) :
+		
+	#	print( 'Coordonnées ({},{}) envoyées à {}:{}'.format( x, y, *chost ) )
+		clnt = liste_client[ chost ]
+		clnt.send( '{},{}.\n'.format( x-n*sw, y ) )
+
+	# Timer
+	time.sleep( 0.03 )
