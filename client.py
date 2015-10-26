700dad7d7e30febd09a91a1ad538821947966066
Michaël Roy <microygh@gmail.com>
2015-10-26 14:54:20 +0100

    Replace the Tkinter interface by a Qt one.

--- client.py
+++ client.py
@@ -13,7 +13,8 @@
 import socket
 import sys
 import threading
-from Tkinter import *
+from PyQt4 import QtCore
+from PyQt4 import QtGui
 
 
 #
@@ -24,29 +25,23 @@ class Ball( threading.Thread ) :
 	#
 	# Initialization
 	#
-	def __init__( self, server, can, root ) :
+	def __init__( self, server, parent ) :
 
 		# Initialize the thread
 		threading.Thread.__init__( self )
 		
 		# Graphical component
-		self.can = can
-		self.root = root
+		self.parent = parent
 
 		# Create Internet TCP socket
 		self.connection = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		self.connection.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 
 		# Network connection
-		try :
-
-			self.connection.connect( ( server, 10000 ) )
-
+		try : self.connection.connect( ( server, 10000 ) )
 		except :
-
 			print( 'Connection failed...' )
 			sys.exit()    
-
 		print( 'Connected to the server...' )
 
 	#
@@ -54,9 +49,8 @@ class Ball( threading.Thread ) :
 	#
 	def run( self ) :
 		
-		self.running = True
-
 		# Main loop
+		self.running = True
 		while self.running :
 
 			msg = self.connection.recv( 256 )
@@ -67,30 +61,87 @@ class Ball( threading.Thread ) :
 			x = int( sx )
 			y = int( sy )
 		#	print( 'x = {}  ~  y = {}'.format( x, y ) )
-			self.can.coords( balle, x-30, y-30, x+30, y+30 )
-			self.root.update()
+			# Send the image to the widget through a signal
+			self.parent.ball_position_updated.emit( x, y )
+			
+		print( 'Connection closed...' )
+		self.connection.close()
+		self.parent.close()
 
 
+#
+# Widget to display the ball
+#
+class Widget( QtGui.QWidget ) :
+	
+	#
+	# Signal to receive the new ball position from the server
+	#
+	ball_position_updated = QtCore.pyqtSignal( int, int )
 
-# Graphical user interface
-root = Tk()
-sw = root.winfo_screenwidth()
-sh = root.winfo_screenheight()
-root.geometry( '{}x{}+0+0'.format( sw, sh ) )
-can = Canvas( root, width =sw, height=sh-100, bg='white' )
-can.pack()
-balle = can.create_oval( 0, 0, 0, 0, fill = 'red' )
-Button( root, text = 'Quit', command = root.quit ).pack( side = BOTTOM, pady = 30 )
+	#
+	# Initialization
+	#
+	def __init__( self, parent = None ) :
 
-# Ball thread
-try :
-	ball_thread = Ball( sys.argv[ 1 ], can, root )
-	ball_thread.start()
-	root.mainloop()
-except :
-	ball_thread.running = False
-	ball_thread.join()
-	ball_thread.connection.close()
+		# Initialize QLabel
+		super( Widget, self ).__init__( parent )
 
-        
-        
+		# Change the window title
+		self.setWindowTitle( 'NetworkDemo' )
+		
+		# Change the widget position and size
+		self.setGeometry( 0, 0, 1920, 1200 )
+		
+		# Set the Escape key to close the application
+		QtGui.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_Escape ), self ).activated.connect( self.close )
+		
+		# Ball thread
+		self.ball_thread = Ball( sys.argv[ 1 ], self )
+		self.ball_thread.start()
+		
+		# Ball Position
+		self.ball_position = QtCore.QPoint()
+
+		# Connect the signal to update the ball position
+		self.ball_position_updated.connect( self.UpdateBallPosition )
+		
+	#
+	# Update the ball position
+	#
+	def UpdateBallPosition( self, x, y ) :
+		
+		self.ball_position.setX( x )
+		self.ball_position.setY( y )
+		self.update()
+
+	#
+	# Paint the ball
+	#
+	def paintEvent( self, event ) :
+		
+		paint = QtGui.QPainter( self )
+		paint.setRenderHint( QtGui.QPainter.Antialiasing )
+		paint.setBrush( QtCore.Qt.red )
+		paint.drawEllipse( self.ball_position, 60, 60 )
+
+	#
+	# Close the widget
+	#
+	def closeEvent( self, event ) :
+
+		self.ball_thread.running = False
+		self.ball_thread.join()
+		event.accept()
+
+
+#
+# Main application
+#
+if __name__ == '__main__' :
+
+	application = QtGui.QApplication( sys.argv )
+	widget = Widget()
+	widget.show()
+	sys.exit( application.exec_() )
+
