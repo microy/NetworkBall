f38ae69fae977b7a5a82d8dec56689736228b578
Michaël Roy <microygh@gmail.com>
2015-11-19 20:25:05 +0100

    Cleanup.

--- Client.py
+++ Client.py
@@ -1,166 +1,109 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 #
 # Client side of the NetworkDemo application
-#
 # Receive the ball from the server, and display it
 #
 
-
-#
 # External dependencies
-#
 import socket
 import sys
 import threading
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 
-
-#
 # Class to receive the ball position from the server (threaded)
-#
 class BallClient( threading.Thread ) :
-	
-	#
-	# Initialization
-	#
-	def __init__( self, server, widget ) :
 
+	# Initialization
+	def __init__( self, server, widget ) :
 		# Initialize the thread
 		super( BallClient, self ).__init__()
-		
 		# Server address
 		self.server = server
-
 		# Graphical user interface
 		self.widget = widget
-
 		# Ball position
 		self.position = [ 0, 0 ]
 
-	#
 	# Thread main loop
-	#
 	def run( self ) :
-		
 		# Connect to the server
 		connection = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		connection.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 		connection.connect( ( self.server, 10000 ) )
-
 		# Continuously receive messages from the server
 		self.running = True
 		while self.running :
-
 			# Receive the message from the server
 			message = connection.recv( 256 )
-			
 			# Server is dead
 			if not message :
-				
 				# Stop the client connection
 				self.running = False
 				break
-				
 			# Decode the message to get the ball position
-			sx, _, sy = message.decode( 'ascii' ).partition( ';' )
-
+			x, _, y = message.decode( 'ascii' ).partition( ';' )
 			# Save the ball position
-			self.position = [ float( sx ), float( sy ) ]
-
+			self.position = [ float( x ), float( y ) ]
 			# Update the widget
 			self.widget.update()
-
 		# Close the connection
 		connection.close()
-
 		# Close the widget
 		self.widget.close()
 
-
-#
 # Widget to display the ball
-#
 class BallWidget( QtGui.QWidget ) :
-	
-	#
-	# Initialization
-	#
-	def __init__( self, parent = None ) :
 
+	# Initialization
+	def __init__( self, parent = None ) :
 		# Initialize the widget
 		super( BallWidget, self ).__init__( parent )
-
 		# Change the window title
 		self.setWindowTitle( 'NetworkDemo' )
-		
 		# Change the widget background color
 		self.setStyleSheet( "background-color:white;" )
-
 		# Set the Escape key to close the application
 		QtGui.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_Escape ), self ).activated.connect( self.close )
-		
 		# Set the F12 key to toggle fullscreen
 		QtGui.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_F12 ), self ).activated.connect( self.ToggleFullScreen )
-		
 		# Ball client thread
 		self.ball = BallClient( sys.argv[ 1 ], self )
 		self.ball.start()
-		
-	#
+
 	# Paint the ball
-	#
 	def paintEvent( self, event ) :
-		
 		# Set up the painter
 		paint = QtGui.QPainter( self )
 		paint.setRenderHint( QtGui.QPainter.Antialiasing )
 		paint.setBrush( QtCore.Qt.red )
-		
 		# Get the ball position
 		position = QtCore.QPoint( self.ball.position[0] * self.size().width(),
 								  self.ball.position[1] * self.size().height() )
-								  
 		# Draw the ball
 		paint.drawEllipse( position, 60, 60 )
 
-	#
 	# Close the widget
-	#
 	def closeEvent( self, event ) :
-
 		# Stop the ball client
 		if self.ball.isAlive() and self.ball.running :
 			self.ball.running = False
 			self.ball.join()
-			
 		# Close the widget
 		event.accept()
-		
-	#
+
 	# Toggle widget fullscreen
-	#
 	def ToggleFullScreen( self ) :
-		
 		# Show normal
-		if self.isFullScreen() :
-			self.showNormal()
-		
+		if self.isFullScreen() : self.showNormal()
 		# Show fullscreen
 		else : self.showFullScreen()
 
-
-#
 # Main application
-#
 if __name__ == '__main__' :
-
-	# Start Qt application
 	application = QtGui.QApplication( sys.argv )
 	widget = BallWidget()
 	widget.show()
 	sys.exit( application.exec_() )
-
--- Server.py
+++ Server.py
@@ -1,117 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 #
 # Server side of the NetworkDemo application
-#
 # Send a ball to all the clients (like a token ring)
 #
 
-
-#
 # External dependencies
-#
 import select
 import socket
 import sys
 import time
 import threading
 
-
-#
 # Class to send the ball position to the clients
-#
 class BallServer( threading.Thread ) :
-	
-	#
+
 	# Server main loop
-	#
 	def run( self ) :
-		
 		# Set up the server connection
 		server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		server.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 		server.bind( ( '', 10000 ) )
 		server.listen( 5 )
-
 		# List of connected clients
 		clients = []
-
 		# Ball coordinates and speed
 		x, y, v, dx, dv = 0.1, 0.1, 0, 0.006, 0.006
-
 		# Continuously accept client connection, and send ball position
 		self.running = True
 		while self.running :
-
 			# New client connection
 			ready, _, _ = select.select( [ server ], [], [], 0 )
 			if ready :
 				client, _ = server.accept()
 				clients.append( client )
-					
 			# Temporize, and continue if there is no client
 			if not clients :
 				time.sleep( 0.1 )
 				continue
-
+			# Invert the ball direction if it reaches a border
+			if x < 0 or x > len( clients ) : dx = -dx
 			# Compute the ball position
-			if x < 0 or x > len( clients ) :
-				dx = -dx
 			x += dx
 			v += dv
-			y += v 
+			y += v
 			if y > 0.95 :
 				y = 0.95
 				v = -v
-
 			# Loop through the client list to send the ball position
 			for n, client in enumerate( clients ) :
-				
 				# Send the coordinates
 				try : client.send( '{};{}\n'.format( x - n, y ).encode( 'ascii' ) )
-				
 				# Client connection error
 				except IOError :
-					
 					# Close the client connection
 					client.close()
-					
 					# Remove the client connection from the client list
 					clients.remove( client )
-					
 					# Fix the ball position
-					if clients and x > len( clients ) :
-						x -= 1
-
+					if clients and x > len( clients ) : x -= 1
 			# Temporization
 			time.sleep( 0.03 )
-			
 		# Close the client connections
 		for client in clients :
 			client.close()
-
 		# Close the server
 		server.close()
 
-
-#
 # Main application
-#
 if __name__ == '__main__' :
-
 	# Start the server
 	server = BallServer()
 	server.start()
-	
 	# Wait for user key press
-	if sys.version_info[0] < 3 :
-		raw_input( 'Press enter to stop the server...' )
-	else : input( 'Press enter to stop the server...' )
-	
+	if sys.version_info[0] < 3 : raw_input( 'Press <enter> to stop the server...' )
+	else : input( 'Press <enter> to stop the server...' )
 	# Stop the server
 	server.running = False
 	server.join()
-
-
